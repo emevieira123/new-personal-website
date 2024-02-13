c578bc44f68642b31b8982ab0a098a095a447f9f
Emerson Vieira <emevieira@gmail.com>
2024-02-13 00:59:23 -0400

    feat: adiciona imagem pessoal a tela inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,50 @@
-import { Divider, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Divider,
+  Flex,
+  Heading,
+  Icon,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { PageAnimate } from "../../shared/components/PageAnimate";
+import { ProfileImage } from "./components/Svg/ProfileImage";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const { colorMode } = useColorMode();
+
   return (
     <PageAnimate>
-      <Flex justify="center">
-        <Flex h="100%" w="80%" flexDirection="column">
+      <Flex h="90vh" justify="space-between" align="center" px="10%">
+        <Flex flexDirection="column">
           <Flex flexDirection="column" padding="1rem 2rem">
             <Text fontWeight="bold">Ola, eu sou</Text>
-            <Heading>Emerson Vieira</Heading>
-            <Text color="gray.500">Desenvolvedor Front-End</Text>
+            <Heading color={colorMode === "dark" ? "green.300" : "green.500"}>
+              Emerson Vieira
+            </Heading>
+            <Text>Desenvolvedor Front-End</Text>
           </Flex>
 
-          <Divider w="25rem" marginLeft="2rem" />
+          <Divider w="23.5rem" marginLeft="2rem" />
 
           <Flex flexDirection="column" padding="1rem 2rem">
             <Text>Gosto de codificar e dar vida as ideias no navegador</Text>
             <Text>desenvolvendo soluções atraentes e intuitivas.</Text>
           </Flex>
         </Flex>
+
+        <motion.div
+          initial={{ opacity: 0, y: 200, scale: 0.5 }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: 200, scale: 0.5 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Icon
+            as={ProfileImage}
+            fontSize={400}
+            color={colorMode === "dark" ? "green.300" : "green.500"}
+          />
+        </motion.div>
       </Flex>
     </PageAnimate>
   );
--- src/shared/components/PageAnimate/index.tsx
+++ src/shared/components/PageAnimate/index.tsx
@@ -5,6 +5,7 @@ export function PageAnimate({ children }: { children: React.ReactNode }) {
     <motion.div
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.5 }}
     >
       {children}
